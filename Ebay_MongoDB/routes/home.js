c63e9bd3a0cb977d02ced8fc3e173b9bb285bd23
TheBloodMage <nachiket663@gmail.com>
2016-10-30 01:48:29 -0700

    RabbitMQ basic implemented
    1. Established a Simple basic rabbitMQ connection and implemented the
    login service call with it.

--- Ebay_MongoDB/routes/home.js
+++ Ebay_MongoDB/routes/home.js
@@ -5,6 +5,7 @@ var bcrypt = require('bcrypt');
 var mongo = require("./mongo");
 var ObjectId = require('mongodb').ObjectID;
 var mongoURL = "mongodb://localhost:27017/EbayDatabaseMongoDB";
+var mq_client = require('../rpc/client');
 
 exports.signin = function(req, res) {
 
@@ -40,6 +41,39 @@ exports.registerNewUser = function(req, res) {
 	var last_name = req.param("last_name");
 	var inputUsername = req.param("inputUsername");
 	var inputPassword = hash;
+	
+	var msg_payload = { 
+			"first_name": first_name, 
+			"last_name": last_name,
+			"inputUsername": inputUsername, 
+			"inputPassword": inputPassword
+	};
+	
+	console.log("Adding A POST Request with msg_payload as: " + msg_payload);
+	console.log(msg_payload);
+	
+	mq_client.make_request('test_register_queue', msg_payload, function(err,results){
+		
+		console.log(results);
+		if(err){
+			throw err;
+		}
+		else 
+		{
+			if(results.code == 200){
+				console.log("valid Login");
+				
+				res.send({"login":"Success"});
+			}
+			else {    
+				
+				console.log("Invalid Login");
+				res.send({"login":"Fail"});
+			}
+		}  
+	});
+	
+	
 
 	mongo.connect(mongoURL, function() {
 		console.log('Connected to mongo at: ' + mongoURL);
